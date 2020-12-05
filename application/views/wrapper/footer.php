3ca5b85cfcdebe26792ee0519bcf8b7747ec97a1
Eko Cahyanto <49233072+cahya93@users.noreply.github.com>
2020-12-06 01:51:12 +0700

    Update footer.php

--- application/views/wrapper/footer.php
+++ application/views/wrapper/footer.php
@@ -25,18 +25,18 @@
 
 
      <!-- Bootstrap core JavaScript-->
-     <script src="<?= base_url(); ?>vendor/jquery/jquery.min.js"></script>
-     <script src="<?= base_url(); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+     <script src="<?= base_url(); ?>assets/jquery/jquery.min.js"></script>
+     <script src="<?= base_url(); ?>assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
      <!-- Core plugin JavaScript-->
-     <script src="<?= base_url(); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+     <script src="<?= base_url(); ?>assets/jquery-easing/jquery.easing.min.js"></script>
 
      <!-- Custom scripts for all pages-->
      <script src="<?= base_url(); ?>assets/js/sb-admin-2.min.js"></script>
 
      <!-- Page level plugins -->
-     <script src="<?= base_url(); ?>vendor/datatables/jquery.dataTables.min.js"></script>
-     <script src="<?= base_url(); ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
+     <script src="<?= base_url(); ?>assets/datatables/jquery.dataTables.min.js"></script>
+     <script src="<?= base_url(); ?>assets/datatables/dataTables.bootstrap4.min.js"></script>
 
      <!-- Page level custom scripts -->
      <script src="<?= base_url(); ?>assets/js/demo/datatables-demo.js"></script>
