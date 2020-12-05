6a74546f46ea534f0d9de90a09d19fd566806f4d
Eko Cahyanto <49233072+cahya93@users.noreply.github.com>
2020-12-06 01:39:42 +0700

    Update footer.php

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -13,16 +13,16 @@
 </a>
 <!-- Optional JavaScript -->
 <!-- Bootstrap core JavaScript-->
-<script src="<?= base_url(); ?>vendor/jquery/jquery.min.js"></script>
-<script src="<?= base_url(); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url(); ?>assets/jquery/jquery.min.js"></script>
+<script src="<?= base_url(); ?>assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="<?= base_url(); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?= base_url(); ?>assets/jquery-easing/jquery.easing.min.js"></script>
 
 
 <!-- Page level plugins -->
-<script src="<?= base_url(); ?>vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="<?= base_url(); ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="<?= base_url(); ?>assets/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url(); ?>assets/datatables/dataTables.bootstrap4.min.js"></script>
 <!-- Load librari/plugin jquery nya -->
 <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>" type="text/javascript"></script>
 
