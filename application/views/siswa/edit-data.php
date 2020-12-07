e3ff4dd721627ec529891f890c0ea4c781f2805b
Eko Cahyanto <49233072+cahya93@users.noreply.github.com>
2020-12-07 12:25:39 +0700

    Update edit-data.php

--- application/views/siswa/edit-data.php
+++ application/views/siswa/edit-data.php
@@ -71,7 +71,7 @@
                         <select name="jurusan" id="jurusan">
                             <option value="">Pilih Jurusan</option>
                             <?php foreach ($iduka as $d) : ?>
-                                <option value="<?= $d['singkatan_jurusan']; ?>"><?= $d['jurusan']; ?></option>
+                                <option value="<?= $d['jurusan']; ?>"><?= $d['jurusan']; ?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
