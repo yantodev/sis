cd68fd25054e8abf68fcafb54c9b63f1b9428db0
Eko Cahyanto <49233072+cahya93@users.noreply.github.com>
2020-12-19 12:21:26 +0700

    juknis
    Jukni

--- application/views/home/data.php
+++ application/views/home/data.php
@@ -49,4 +49,5 @@
             </div>
         </div>
     </div>
+    <iframe width="100%" height="315" src="https://www.youtube.com/embed/mHXj5kVIHvg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
--- application/views/home/navbar.php
+++ application/views/home/navbar.php
@@ -10,8 +10,14 @@
                 <li class="nav-item active">
                     <a class="nav-link" href="<?= base_url('home/data'); ?>">Home <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Juknis
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                        <a class="dropdown-item" href="https://youtu.be/mHXj5kVIHvg">Membuat Akun</a>
+                    </div>
+                </li>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
