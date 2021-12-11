bc92b9f434b4544a886f385adf4c7c30de4097db
yantodev <ekocahyanto007@gmail.com>
2021-12-11 11:50:04 +0700

    modified navbar

--- application/views/home/wrapper/navbar.php
+++ application/views/home/wrapper/navbar.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary mb-3">
         <a class="navbar-brand" href="<?= base_url(''); ?>">APPS PKL</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -10,7 +10,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="<?= base_url('home/data'); ?>">Home <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item dropdown">
+                <!-- <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Juknis
                     </a>
@@ -18,7 +18,7 @@
                         <a class="dropdown-item" href="https://youtu.be/mHXj5kVIHvg">Membuat Akun</a>
                         <a class="dropdown-item" href="https://youtu.be/omf5jK0DVrk">Surat Balasan Akun</a>
                     </div>
-                </li>
+                </li> -->
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -37,7 +37,7 @@
                     <a class="nav-link" href="<?= base_url('home/d_hadir'); ?>">Daftar Hadir</a>
                 </li>
             </ul>
-            <a href="<?= base_url('auth'); ?>"><button class="btn btn-outline-success my-2 my-sm-0">LOGIN</button></a>
+            <a href="<?= base_url('auth'); ?>"><button class="btn btn-outline-warning my-2 my-sm-0">LOGIN</button></a>
         </div>
     </nav>
 </div>
